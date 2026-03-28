72a1a053c794503acc234fc0dcdc5b1cb484329f
Hampton Brewer <hpbrew@gmail.com>
2026-03-28 08:05:36 -0500

    adjust url pointer for build

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/japanese-mastery/" : "/",
+  build: { outDir: 'dist' },
   plugins: [
     vue(),
     vueJsx(),
